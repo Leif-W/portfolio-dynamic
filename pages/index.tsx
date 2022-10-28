5c853b101adc3a02bb07347cc4c331032dcbba0c
Leif-W <Leif-W@noreply.users.github.com>
2022-10-28 09:44:34 -0400

    Init page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,13 @@ import Hero from '../components/Hero';
 
 export default function Home({}: NextPage) {
 	return (
-		<div>
+		<div
+			className='
+				bg-[#171717]
+				h-screen
+				text-[#0f0]
+			'
+		>
 			<Head>
 				<title>Leif Westerlind&apos;s Dynamic Portfolio</title>
 				</Head>
