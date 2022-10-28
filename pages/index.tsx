b08ea11f24c3d1c499cf615a89a3daa8e08c7a22
Leif-W <Leif-W@noreply.users.github.com>
2022-10-27 23:28:35 -0400

    Outline index components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-const Home: NextPage = () => {
+export default function Home({}: NextPage) {
   return (
     <div>
       <Head>
         <title>Leif Westerlind&apos;s Dynamic Portfolio</title>
       </Head>
 
-      <h1 className="p-10 text-red-500">Let&apos;s build an AWESOME portfolio!</h1>
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact */}
+
     </div>
   );
 };
-
-export default Home;
