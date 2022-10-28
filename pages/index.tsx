d296c5c55fd54998524572d36661ca74c9680888
Leif-W <Leif-W@noreply.users.github.com>
2022-10-28 07:46:48 -0400

    Init Hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 export default function Home({}: NextPage) {
   return (
@@ -13,7 +14,9 @@ export default function Home({}: NextPage) {
       <Header />
 
       {/* Hero */}
-
+      <section id='hero'>
+        <Hero />
+      </section>
       {/* About */}
 
       {/* Experience */}
