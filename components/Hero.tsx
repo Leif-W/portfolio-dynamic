67a00d2745cc9f105f9c62d99702f71655bb9403
Leif-W <Leif-W@noreply.users.github.com>
2022-10-28 09:48:55 -0400

    Add react-simple-typewriter to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
 
 type Props = {};
 
-export default function Hero({}: Props) {
+export default function Hero({ }: Props) {
+	const [text, count] = useTypewriter({
+		delaySpeed: 1500,
+		loop: true,
+		words: [
+			'Leif Westerlind',
+			'Developer',
+			'Designer',
+			'Hacker',
+		],
+	});
+
 	return (
-		<div>Hero</div>
+		<div>
+			<h1>
+				<span>
+					{text}
+					<Cursor cursorColor='#0F0' />
+				</span>
+			</h1>
+		</div>
 	);
 };
