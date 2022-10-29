5a865daa9130d57eacb4de9d2ef0c6313663af25
Leif-W <Leif-W@noreply.users.github.com>
2022-10-28 22:57:09 -0400

    Init About section and component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
 export default function Home({}: NextPage) {
 	return (
@@ -34,6 +35,14 @@ export default function Home({}: NextPage) {
 			</section>
 
 			{/* About */}
+			<section
+				className='
+					snap-center
+				'
+				id='about'
+			>
+				<About />
+			</section>
 
 			{/* Experience */}
 
