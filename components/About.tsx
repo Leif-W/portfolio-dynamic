d429c7826e849e35ab98834adb584132fec88f50
Leif-W <Leif-W@noreply.users.github.com>
2022-10-28 22:58:03 -0400

    Init About style

--- components/About.tsx
+++ components/About.tsx
@@ -4,6 +4,21 @@ type Props = {};
 
 export default function About({}: Props) {
 	return (
-		<div>About</div>
+		<div
+			className='
+				flex
+				flex-col
+				h-screen
+				items-center
+				justify-evenly
+				max-w-7xl
+				md:flex-row
+				md:text-left
+				mx-auto
+				px-10
+				relative
+				text-center
+			'
+		>About</div>
 	);
 };
