cc09141a1d89003e52adc7596c9eb92297eca73c
Leif-W <Leif-W@noreply.users.github.com>
2022-10-30 00:50:52 -0400

    Style justify text in About

--- components/About.tsx
+++ components/About.tsx
@@ -109,6 +109,7 @@ export default function About({}: Props) {
 					className='
 						text-[#2F2]
 						text-base
+						text-justify
 					'
 				>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
