3c460755b5e85e88ec68010822e3fb2665219e70
Leif-W <Leif-W@noreply.users.github.com>
2022-10-30 02:25:31 -0400

    Init structure of ExperienceCard

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,73 @@
 import React from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 export default function ExperienceCard({}: Props) {
 	return (
-		<div>ExperienceCard</div>
+		<article>
+			<motion.div
+				className='
+					h-32
+					md:h-40
+					md:w-40
+					relative
+					w-32
+					xl:h-48
+					xl:w-48
+				'
+				initial={{
+					opacity: 0,
+					y: -100,
+				}}
+				transition={{ duration: 0.75 }}
+				viewport={{ once: false }}
+				whileInView={{
+					opacity: 1,
+					y: 0,
+				}}
+			>
+				<Image
+					alt='portrait'
+					className='
+						object-center
+						object-contain
+						rounded-full
+					'
+					fill
+					sizes='
+						(max-width: 768px) 8rem,
+              			(max-width: 1200px) 10rem,
+              			12rem
+					'
+					src='/Warp9pnt9.png'
+				/>
+				<h4
+					className='
+						absolute
+						text-[#0F0]
+						text-2xl
+						top-24
+						tracking-[1.25rem]
+						uppercase
+					'
+				>CEO of Company, Inc.</h4>
+				<span>Company, Inc.</span>
+				<span>
+					{/* Tech icon */}
+					{/* Tech icon */}
+					{/* Tech icon */}
+				</span>
+				<span>2002 - 2022</span>
+				<ul>
+					<li>Summary points</li>
+					<li>Summary points</li>
+					<li>Summary points</li>
+					<li>Summary points</li>
+					<li>Summary points</li>
+				</ul>
+			</motion.div>
+		</article>
 	);
 };
