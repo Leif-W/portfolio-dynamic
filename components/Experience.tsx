d1851832b4e5c80d7b31eee480265fa0ca1eaec0
Leif-W <Leif-W@noreply.users.github.com>
2022-10-30 00:53:14 -0400

    Init Experience component

---
+++ components/Experience.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {};
+
+export default function Experience({}: Props) {
+	return (
+		<div
+			className='
+				h-screen
+				text-[#0F0]
+			'
+		>Experience</div>
+	);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
+import Experience from '../components/Experience';
 
 export default function Home({}: NextPage) {
 	return (
@@ -44,6 +45,14 @@ export default function Home({}: NextPage) {
 			</section>
 
 			{/* Experience */}
+			<section
+				className='
+					snap-center
+				'
+				id='experience'
+			>
+				<Experience />
+			</section>
 
 			{/* Skills */}
 
