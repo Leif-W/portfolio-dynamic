8a887e1ae7b54dd385757709d972e30ecc8e3d3f
Leif-W <Leif-W@noreply.users.github.com>
2022-10-30 01:21:31 -0400

    Style Experience component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 export default function Experience({}: Props) {
 	return (
-		<div
+		<motion.div
 			className='
+				flex
+				flex-col
 				h-screen
-				text-[#0F0]
+				items-center
+				justify-evenly
+				max-w-full
+				md:flex-row
+				mx-auto
+				overflow-hidden
+				px-10
+				relative
+				text-left
 			'
-		>Experience</div>
+			initial={{ opacity: 0 }}
+			transition={{ duration: 1.5 }}
+			whileInView={{ opacity: 1 }}
+		>
+			<h3
+				className='
+					absolute
+					text-[#0F0]
+					text-2xl
+					top-24
+					tracking-[1.25em]
+					uppercase
+				'
+			>Experience</h3>
+			<div>
+				{/* ExperienceCard */}
+				{/* ExperienceCard */}
+				{/* ExperienceCard */}
+			</div>
+		</motion.div>
 	);
 };
